2543f10f5c7eb575039a6ea92d8b3465104accd2
Ryan Huang <59319094+ryan-huang1@users.noreply.github.com>
2022-11-29 17:37:17 -0500

    Changes Made 11/29/22
    Added arcade drive cause ryan removed it like a dumb dumb
    
    Some other stuff

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,6 +6,7 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.commands.ArcadeDrive;
 import frc.robot.subsystems.Drivetrain;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -17,10 +18,13 @@ import edu.wpi.first.wpilibj2.command.Command;
  * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
  * subsystems, commands, and button mappings) should be declared here.
  */
+import edu.wpi.first.wpilibj.Joystick;
+
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final Drivetrain m_drivetrain = new Drivetrain();
   // TO-DO Declare a joystick
+  public Joystick driverController = new Joystick(Constants.DRIVER_Controller);
 
   // Create SmartDashboard chooser for autonomous routines
   private final SendableChooser<Command> m_chooser = new SendableChooser<>();
@@ -41,7 +45,6 @@ public class RobotContainer {
     // Default command is arcade drive. This will run unless another command
     // is scheduled over it.
     m_drivetrain.setDefaultCommand(getArcadeDriveCommand());
-
     SmartDashboard.putData(m_chooser);
   }
 
@@ -53,13 +56,13 @@ public class RobotContainer {
   public Command getAutonomousCommand() {
     return m_chooser.getSelected();
   }
-
+  public Command getArcadeDriveCommand()
+  {   return new ArcadeDrive(
+      m_drivetrain, () -> -driverController.getRawAxis(1), () -> driverController.getRawAxis(2));
+  }
   /**
    * Use this to pass the teleop command to the main {@link Robot} class.
    *
    * @return the command to run in teleop
    */
-  public Command getArcadeDriveCommand() {
-    //return arcade drive command
-  }
 }
--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -5,23 +5,33 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Joystick;
 
 public class Drivetrain extends SubsystemBase {
 
   // TO-DO Create two PWMTalonSRX motor controllers
-
+  Spark LeftMotor = new Spark(0);
+  Spark RightMotor = new Spark(1);
+  Encoder LeftEncoder = new Encoder(4, 5);
+  Encoder RightEncoder = new Encoder(6, 7);
 
   // TO-DO Set up the differential drive controller
-
+  DifferentialDrive diffDrive = new DifferentialDrive(LeftMotor, RightMotor);
   /** Creates a new Drivetrain. */
   public Drivetrain() {
     // We need to invert one side of the drivetrain so that positive voltages
     // result in both sides moving forward. Depending on how your robot's
     // gearbox is constructed, you might have to invert the left side instead.
+    LeftMotor.setInverted(true);
   }
 
   public void arcadeDrive(double xaxisSpeed, double zaxisRotate) {
     //TO-DO Call the arcade drive method
+    diffDrive.arcadeDrive(xaxisSpeed, zaxisRotate);
   }
 
   @Override
